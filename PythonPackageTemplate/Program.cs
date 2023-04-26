508253bd2deae1117f63542bf79b5938e1bfba95
duoykb <duoykb@gmail.com>
2023-04-26 18:36:42 +0300

    initial

---
+++ PythonPackageTemplate/Program.cs
@@ -0,0 +1,65 @@
+﻿string rootDir = Ask("root dir");
+string projName = Ask("proj name");
+List<(string content, string f)> templates = new()
+{
+    ("", "README.md"),
+    ($""""
+        [metadata]
+        name = package-name
+        version = 1.0.0
+        author = author-name
+        author_email = None
+        description = A small example package
+        long_description = file: README.md
+        long_description_content_type = text/markdown
+        url = https://example.com
+        project_urls =
+        Docs = https://docs.example.com
+        classifiers =
+        { "\t"} Programming Language :: Python :: 3
+        { "\t"} License :: OSI Approved :: MIT License
+        { "\t"} Operating System :: OS Independent 
+        [options]
+        package_dir =
+        { "\t"} = src
+        packages = find:
+        python_requires = >=3.6
+
+        [options.packages.find]
+        where = src 
+        """" , "setup.cfg"),
+    (""""   
+    [build-system]\nrequires = [
+    "setuptools>=42",
+    "wheel"
+    ]
+    build-backend = "setuptools.build_meta"
+    """", "pyproject.toml")
+};
+
+string Ask(string q)
+{
+    Write($"{q}: ");
+    return ReadLine() ?? throw new ArgumentException("Path can't be null");
+}
+
+void CreateTemplate()
+{
+    try
+    {
+        var projDir = Path.Combine(rootDir, projName);
+        var srcDir = Path.Combine(projDir, "src");
+        var testsDir = Path.Combine(projDir, "tests");
+        Directory.CreateDirectory(projDir);
+        Directory.CreateDirectory(srcDir);
+        Directory.CreateDirectory(testsDir);
+
+        foreach (var (content, f) in templates) File.WriteAllText(Path.Combine(projDir, f), content);
+    }
+    catch (IOException)
+    {
+        WriteLine("Failed");
+    }
+}
+
+CreateTemplate();
